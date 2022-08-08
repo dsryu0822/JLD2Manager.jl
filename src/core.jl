c2bec43d63188c8f808366542120fe8a40a85d9c
rmsmsgood <rmsmsgood@naver.com>
2022-08-08 19:24:19 +0900

    code functionized

--- src/core.jl
+++ src/core.jl
@@ -33,9 +33,77 @@ Available Commands:
 
 # clipboard()
 
-function show_list(jld2file)
-    for (idx, key) in enumerate(keys(jld2file))
-        println("$key =>")
-        println(jld2file[key])
-    end
+function show_ls(jld2file)
+	idxl = jld2file |> keys |> length |> log10 |> ceil |> Int
+	keyl = jld2file |> keys .|> length |> maximum
+	filekeys = keys(jld2file)
+	# textwidth
+	println((" "^idxl) *" "* lpad("key", keyl) * lpad("size", 16) * lpad("type", 16) * lpad("preview", 16))
+	for (idx, key) in enumerate(filekeys)
+		data = jld2file[key]
+		datatype = typeof(data)
+		print(lpad(idx, idxl) * " "
+			* lpad(key, keyl)
+			* lpad(sizeof(data), 16)
+			* lpad(datatype, 16)
+			);
+		prvw = (datatype <: Number) ? lpad("$(trunc(data, digits = 6))", 16) : lpad("", 16)
+		println(prvw)
+	end
+end
+
+function read(directory, filename, jld2list)
+    jldopen(joinpath(directory, filename)) do jld2file
+		filekeys = keys(jld2file)
+		input = ""
+
+		# for (idx, key) in enumerate(filekeys)
+		# 	if idx != maximum(idx)
+		# 		println("├─ " * lpad(idx, idxl) *" "* lpad(key, keyl))
+		# 	else
+		# 		println("└─ " * lpad(idx, idxl) *" "* lpad(key, keyl))
+		# 	end
+		# end
+    
+		page = 0
+		while input != "exit"
+			page += 1
+			if input in ["", "h", "q", "pwd"]
+				if input == ""
+					println("page: $page")
+				elseif input == "h"
+					println(help_message)
+				elseif input == "q"
+					break
+				elseif input == "pwd"
+					println(directory)
+				end
+			elseif input ∈ ["ls"]
+				show_ls(jld2file)
+			elseif prod(isdigit.([a for a in input]))
+				idx = parse(Int, input)
+				data = jld2file[filekeys[idx]]
+				show(IOContext(stdout, :limit => true), "text/plain", data)
+			elseif input ∈ filekeys
+				data = jld2file[input]
+				show(IOContext(stdout, :limit => true), "text/plain", data)
+			elseif input == "."
+				for key in filekeys
+					data = jld2file[key]
+					print(Crayon(foreground = (194,94,95)), key)
+					println(Crayon(reset=true), "")
+					show(IOContext(stdout, :limit => true), "text/plain", data)
+				end
+			elseif input in ["n", "p"]
+				newfileidx = mod(findfirst(filename .== jld2list) + ifelse(input == "p", -1, 1) - 1, length(jld2list)) + 1
+				return jld2list[newfileidx]
+			else
+				println("page: $page")
+			end
+			println("   (h / ls / q)\n")
+			print(Crayon(foreground = (194,94,95)), "JLD2>")
+			print(Crayon(reset=true), " ")
+			input = readline()
+		end
+	end
 end
--- src/main.jl
+++ src/main.jl
@@ -1,90 +1,35 @@
-using JLD2, DataFrames, Crayons
+using JLD2, DataFrames, CSV, Crayons
 
 include("core.jl")
 
-directory = ""
+abspath = []
+push!(abspath, ARGS[1])
+while true
+    # for _ in 1:5
+    #     if ARGS |> isempty
+    #         try
+    #             using Gtk
+    #             abspath = open_dialog_native("JLD2", GtkNullContainer(), ["*.jld2"]);
+    #             break
+    #         catch LoadError
+    #             @error "There is no jld2 file to read, so we want to find your file via interactive dialog. Please install `Gtk.jl` package in current julia: Run `import Pkg; Pkg.add(\"Gtk\")` to install the Gtk package."
+    #             run(`julia`)
+    #         end
+    #     else
+    #         abspath = ARGS[1]
+    #         break
+    #     end
+    #     if abspath == "" continue end
+    # end
 
-for _ in 1:5
-    if ARGS |> isempty
-        try
-            using Gtk
-            global directory = open_dialog_native("JLD2", GtkNullContainer(), ["*.jld2"]);
-            break
-        catch LoadError
-            @error "There is no jld2 file to read, so we want to find your file via interactive dialog. Please install `Gtk.jl` package in current julia: Run `import Pkg; Pkg.add(\"Gtk\")` to install the Gtk package."
-            run(`julia`)
-        end
-    else
-        global directory = ARGS[1]
-        break
-    end
-    if directory == "" continue end
+    directory = dirname(last(abspath))
+    filename = basename(last(abspath))
+    jld2list = readdir(directory)[(last.(split.(readdir(directory), '.')) .== "jld2")]
+    # show(IOContext(stdout, :limit => true), "text/plain", jld2list)
+    display(jld2list)
+    fileidx = findfirst(filename .== jld2list)
+    println("    [ $fileidx / $(length(jld2list)) ]")
+    
+    # filename = read(directory, filename, jld2list)
+    push!(abspath, joinpath(directory, read(directory, filename, jld2list)))
 end
-
-println(readdir("directory/.."))
-
-# while true
-jldopen(directory) do jld2file
-    # println(filesize(directory))
-    # println("\nPlease type `ls`.")
-
-    command = "h"
-    idxl = jld2file |> keys |> length |> log10 |> ceil |> Int
-    keyl = jld2file |> keys .|> length |> maximum
-    filekeys = keys(jld2file)
-
-    no_ask = 0
-    for (idx, key) in enumerate(filekeys)
-        if idx != maximum(idx)
-            println("├─ " * lpad(idx, idxl) *" "* lpad(key, keyl))
-        else
-            println("└─ " * lpad(idx, idxl) *" "* lpad(key, keyl))
-        end
-    end
-    while command != "exit"
-        # println(command)
-        if command ∈ ["help", "h", "?"]
-            println(help_message)
-        elseif command ∈ ["ls"]
-            # textwidth
-            println((" "^idxl) *" "* lpad("key", keyl) * lpad("size", 16) * lpad("type", 16))
-            for (idx, key) in enumerate(filekeys)
-                data = jld2file[key]
-                println(lpad(idx, idxl) * " "
-                    * lpad(key, keyl)
-                    * lpad(sizeof(data), 16)
-                    * lpad(typeof(data), 16)
-                    );
-            end
-        elseif prod(isdigit.([a for a in command]))
-            idx = parse(Int, command)
-            data = jld2file[filekeys[idx]]
-            # display(data)
-            show(IOContext(stdout, :limit => true), "text/plain", data)
-            # println(repr(MIME("text/plain"), data))
-        elseif command ∈ filekeys
-            data = jld2file[command]
-            # println(repr(MIME("text/plain"), data))
-            show(IOContext(stdout, :limit => true), "text/plain", data)
-        elseif command == "q"
-            println("exit...")
-            break
-        elseif command == "."
-            for key in filekeys
-                data = jld2file[key]
-                print(Crayon(foreground = (194,94,95)), key)
-                println(Crayon(reset=true), "")
-                println(summary(data))
-                show(data)
-            end
-        else
-            no_ask += 1
-            println("no ask: $no_ask")
-        end
-        println("\n    (h / ls / q)\n")
-        print(Crayon(foreground = (194,94,95)), "JLD2>")
-        print(Crayon(reset=true), " ")
-        command = readline()
-    end
-end
-# end
