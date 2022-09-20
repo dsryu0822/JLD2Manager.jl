4921dc92733d8e1ecfaee8c0ca11faaad9a21d5b
rmsmsgood <rmsmsgood@naver.com>
2022-09-20 16:17:34 +0900

    unactive command lines

--- src/JLD2Manager.jl
+++ src/JLD2Manager.jl
@@ -6,7 +6,7 @@ println(pwd())
 println("please run 'powersehll' or 'window terminal' as administrator")
 println("       run 'julia'")
 println(" type that 'using JLD2Manager' in REPL")
-run(`icacls .\\src\\JLD2manager.bat /GRANT Everyone:F`)
-run(`cmd /c assoc .jld2=JLD2Manager.bat`)
+# run(`icacls .\\src\\JLD2manager.bat /GRANT Everyone:F`)
+# run(`cmd /c assoc .jld2=JLD2Manager.bat`)
 
 end # module
